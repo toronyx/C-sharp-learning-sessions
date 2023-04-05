258621d076ccc189bb2563404357c0d6cae04b7a
tr3 <t.ronayne@fnc.co.uk>
2023-04-05 13:15:44 +0100

    initial commit

---
+++ RemoteControlCarConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using RemoteControlCar;
+
+RemoteControlCar.RemoteControlCar nitro = new(50, 0.04f);
+
+Console.WriteLine(nitro);
---
+++ RemoteControlCarConsoleApp/RemoteControlCar.cs
@@ -0,0 +1,25 @@
+﻿namespace RemoteControlCar
+{
+    public class RemoteControlCar
+    {
+        private float speed;
+        private float batteryDrainRate;
+        private float batteryPercentage;
+
+        public RemoteControlCar(float speed, float batteryDrain) 
+        {
+            this.speed = speed;
+            this.batteryDrainRate = batteryDrain;
+            this.batteryPercentage = 1f;
+        }
+
+        public void DrainBattery()
+        {
+            batteryPercentage -= batteryDrainRate;
+            if (batteryPercentage < 0f)
+            {
+                batteryPercentage = 0f;
+            }
+        }
+    }
+}
