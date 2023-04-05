5dc846ca0baaa1f6cf1e9c4fd766f92f307fd325
tr3 <t.ronayne@fnc.co.uk>
2023-04-05 14:50:35 +0100

    Replaced Tom's repo with Katie's

--- RemoteControlCarConsoleApp/RemoteControlCar.cs
+++ RemoteControlCarConsoleApp/RemoteControlCar.cs
@@ -1,25 +1,46 @@
-﻿namespace RemoteControlCar
+﻿namespace Remote_Control_Car_Racing.Vehicles
 {
-    public class RemoteControlCar
+    internal class RemoteControlCar
     {
-        private float speed;
-        private float batteryDrainRate;
+        private readonly float topSpeedMilesPerHour;
+        private readonly float batteryDrainPercPerMile;
         private float batteryPercentage;
+        private float distanceTravelledMiles;
+        private string name = "car";
 
-        public RemoteControlCar(float speed, float batteryDrain) 
+        public RemoteControlCar(float topSpeedMilesPerHour, float batteryDrainPercPerMile, string name)
         {
-            this.speed = speed;
-            this.batteryDrainRate = batteryDrain;
-            this.batteryPercentage = 1f;
+            #region Validation
+            if (topSpeedMilesPerHour < 0) throw new ArgumentOutOfRangeException($"{nameof(topSpeedMilesPerHour)} must not be negative");
+            if (batteryDrainPercPerMile < 0) throw new ArgumentOutOfRangeException($"{nameof(batteryDrainPercPerMile)} must not be negative");
+            #endregion
+
+            this.topSpeedMilesPerHour = topSpeedMilesPerHour;
+            this.batteryDrainPercPerMile = batteryDrainPercPerMile;
+            batteryPercentage = 100;
+            distanceTravelledMiles = 0;
+            this.name = name;
         }
 
-        public void DrainBattery()
+        public string GetCarName()
         {
-            batteryPercentage -= batteryDrainRate;
-            if (batteryPercentage < 0f)
-            {
-                batteryPercentage = 0f;
-            }
+            return name;
         }
+
+        public float MaximumRangeMiles() => batteryDrainPercPerMile / 100;
+
+        public float RemainingRangeMiles() => batteryDrainPercPerMile / batteryPercentage;
+
+        public float Drive(TimeSpan duration)
+        {
+            if (batteryPercentage <= 0) return 0;
+
+            var distanceTravelled = MathF.Min(topSpeedMilesPerHour * (float)duration.TotalHours, RemainingRangeMiles());
+            distanceTravelledMiles += distanceTravelled;
+            batteryPercentage -= batteryDrainPercPerMile * distanceTravelled;
+            return distanceTravelled;
+        }
+
+
     }
 }
