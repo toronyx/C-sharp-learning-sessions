2748b94c69e42f275b89e636e06b6f7a7dc37bcf
tr3 <t.ronayne@fnc.co.uk>
2023-05-10 13:57:45 +0100

    Csharp session on 10th May 2023 - Covered interfaces. This solution is a demonstration/challenge involving interfaces.

---
+++ DogChallenge/DogChallenge/IBark.cs
@@ -0,0 +1,11 @@
+﻿namespace DogChallenge
+{
+    internal interface IBark
+    {
+        public string BarkCommand { get; }
+        public int BarkCount { get; }
+        public string BarkType { get; }
+
+        public bool TryBark();
+    }
+}
---
+++ DogChallenge/DogChallenge/SleepyDog.cs
@@ -0,0 +1,9 @@
+﻿namespace DogChallenge
+{
+    internal class SleepyDog : Dog
+    {
+        public override string BarkType => "zzzzz";
+
+        public override float FetchSpeed => 0f;
+    }
+}
