4373c44c441617d9f239d4f9acbc7db139aea179
tr3 <t.ronayne@fnc.co.uk>
2023-05-16 13:56:00 +0100

    C# learning session 16th May 2023 - Polymorphism and inheritance

---
+++ WizardsAndWarriors/Program.cs
@@ -0,0 +1,21 @@
+﻿using WizardsAndWarriors;
+
+static void PrintDamage(
+    Character character, 
+    Character target)
+{
+    Console.WriteLine(
+        $"{character.Name} did " +
+        $"{character.DamagePoints(target)} damage to " +
+        $"{target.Name}");
+};
+
+Wizard wizard = new("Wizard");
+Warrior warrior = new("Warrior");
+
+PrintDamage(wizard, warrior);
+PrintDamage(warrior, wizard);
+wizard.PrepareSpell();
+Console.WriteLine($"{wizard.Name} is preparing a spell!");
+PrintDamage(warrior, wizard);
+PrintDamage(wizard, warrior);
